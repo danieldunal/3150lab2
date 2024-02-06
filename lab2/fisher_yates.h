23d57c282ed8fbbb252a5f9edcfe1d8a64406b52
danieldunal <144380099+danieldunal@users.noreply.github.com>
2024-02-05 20:37:16 -0500

    Delete lab2 directory

--- lab2/fisher_yates.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FISHER_YATES_H
-#define __FISHER_YATES_H
-
-void print_array(int ary[], int len);
-int swap(int* first, int* second);
-void fisher_yates(int array[], const int length, int (* random_fcn)());
-
-
-#endif
